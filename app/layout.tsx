1862ab223c3aa72b0a92ac6689a7fc778cbee1cf
reichima <rei1gggg@gmail.com>
2026-03-20 01:26:50 +0900

    favicon等サイト設定

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,6 +39,16 @@ export const metadata: Metadata = {
     description: "システム開発のために設計された、ヘッドレスでアクセシブルなReactコンポーネントライブラリ。あなたのプロジェクトを優しく包み込みます。",
     images: "https://gyoza-ui.reichima.com/gyoza.png",
   },
+  icons: {
+    icon: [
+      { url: "/gyoza.svg", type: "image/svg+xml" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    ],
+    shortcut: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
